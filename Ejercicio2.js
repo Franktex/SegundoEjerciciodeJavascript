b096a5e49af665364f08710709f7c53a36917f60
Karina Santillan <mfrancosantillan@gmail.com>
2023-07-04 03:29:46 +0200

    subiendo

---
+++ Ejercicio2.js
@@ -0,0 +1,11 @@
+const Franco = ["Franco", 22, true, new Date (2001, 1, 16)]
+
+const LibroFavorito = {
+    titulo : "HabitosAtomicos",
+    autor : "James Clear",
+    fechaDePublicación : new Date (2018, 9, 16),
+    url: "https://www.amazon.es/Atomic-Habits-Proven-Build-Break/dp/1847941834/ref=sr_1_2?adgrpid=63588341439&hvadid=321774719709&hvdev=c&hvlocphy=9061034&hvnetw=g&hvqmt=e&hvrand=8871670126309143584&hvtargid=kwd-728711807665&hydadcr=14555_1815274&keywords=habitos+atomicos&qid=1688432820&sr=8-2",
+}
+
+console.log(Franco)
+console.log(LibroFavorito)
